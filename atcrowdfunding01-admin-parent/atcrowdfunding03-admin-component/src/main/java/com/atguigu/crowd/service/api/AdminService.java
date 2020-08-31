edc25363bac97b6f788adbf695b6fd3425b055f9
limengjie <li.mengjie@yalla.live>
2020-08-31 17:17:01 +0800

    添加项目

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/service/api/AdminService.java
@@ -0,0 +1,11 @@
+package com.atguigu.crowd.service.api;
+
+import java.util.List;
+
+import com.atguigu.crowd.entity.Admin;
+
+public interface AdminService{
+	void saveAdmin(Admin admin);
+
+	List<Admin> getAll();
+}
